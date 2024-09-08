93e8e6897bac267dc4b48cfb38583d7f399f9d81
nivicius <niviciusdev@gmail.com>
2024-09-08 05:11:59 -0300

    fix: adjusted main file to wortk with use casae

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"go-api/controller"
+	"go-api/usecase"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,10 @@ func main() {
 
 	server := gin.Default()
 
-	ProductController := controller.NewProductControler()
+	ProductUseCase := usecase.NewProductUseCase()
+
+	// controller layer
+	ProductController := controller.NewProductControler(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
