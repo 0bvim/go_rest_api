ea44cb4dfbb1b0caa1c70615288615accb566f69
nivicius <niviciusdev@gmail.com>
2024-09-09 17:45:05 -0300

    feat: struct to connecto in db and started function to create a new API

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,15 @@
 package api
+
+// import necessary packages to api and database connection
+import "database/sql"
+
+// struct of server api
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+// function that return a pointer to an APIServer
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+
+}
