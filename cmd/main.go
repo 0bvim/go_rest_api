8b6a721001e31ba651ccfae1e6159e644ad067e8
nivicius <niviciusdev@gmail.com>
2024-09-09 15:57:52 -0300

    refacto: changing structure of project and rebuilding from scratch

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"go-api/controller"
-	"go-api/usecase"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
 
-	server := gin.Default()
-
-	ProductUseCase := usecase.NewProductUseCase()
-
-	// controller layer
-	ProductController := controller.NewProductControler(ProductUseCase)
-
-	server.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	server.GET("/products", ProductController.GetProducts)
-	server.Run(":8000")
 }
--- model/product.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Product struct {
-	ID    int     `json:"id_product"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
