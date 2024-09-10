ebb256afeeff48d488322f21f3446ded81b67b98
nivicius <niviciusdev@gmail.com>
2024-09-10 00:54:29 -0300

    feat: basic main to run the server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/go_rest_api/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
