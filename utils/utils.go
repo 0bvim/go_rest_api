b21f006b4d034aa5a9c340c875a403d194e0d414
vinicius <niviciusdev@gmail.com>
2024-09-13 19:12:33 -0300

    feat: added parse and write json to  the utils package

---
+++ utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJSON(r *http.Request, payload any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(payload)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
+}
