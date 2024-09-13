4f5baf003f3436e0a593d9a06785d8dc46afdac8
vinicius <niviciusdev@gmail.com>
2024-09-13 19:40:23 -0300

    feat: implemented required types in a string

---
+++ types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=3,max=130"`
+}
