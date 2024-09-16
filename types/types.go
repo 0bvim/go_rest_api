583dd6e8cf553850df9327fbe7fcdd748c1f295c
vinicius <niviciusdev@gmail.com>
2024-09-16 10:25:45 -0300

    feat: user struct

--- types/types.go
+++ types/types.go
@@ -1,5 +1,16 @@
 package types
 
+import "time"
+
+type User struct {
+	FirstName string    `json:"FirstName"`
+	LastName  string    `json:"LastName"`
+	Email     string    `json:"Email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+	ID        int       `json:"id"`
+}
+
 type RegisterUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
