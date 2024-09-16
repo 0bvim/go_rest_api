95ce4bca4fda136935bff7a30de85bc291b445f6
vinicius <niviciusdev@gmail.com>
2024-09-16 18:21:30 -0300

    feat: trying to implement mocked db into api

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -70,7 +70,8 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-	userHandler := user.NewHandler()
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on", s.addr)
